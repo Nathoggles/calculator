6c204116b2aae20a325a75fa12561426c4d8d70d
Nathoggles <mawricus@gmail.com>
2024-09-04 23:28:24 +0200

    set up basic constructor holding calculations

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-script.js
+function createCalc(calcId, num1, operator, num2, result) {
+    const id = {};
+    id.name = calcId;
+    id.num1 = num1;
+    id.operator = operator;
+    id.num2 = num2;
+    id.result = result;
+    return id;
+}
+
+
+const calc1 = (createCalc("calc1", 2, 2, 3));
+console.log(calc1);
