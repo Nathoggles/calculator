269ad45d57ac2fafdc12d36bdf9636c829defc22
Nathoggles <mawricus@gmail.com>
2024-09-04 23:31:11 +0200

    create correct constructor

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function createCalc(calcId, num1, operator, num2, result) {
-    const id = {};
-    id.name = calcId;
-    id.num1 = num1;
-    id.operator = operator;
-    id.num2 = num2;
-    id.result = result;
-    return id;
+function CreateCalc(calcId, num1, operator, num2, result) {
+    this.name = calcId;
+    this.num1 = num1;
+    this.operator = operator;
+    this.num2 = num2;
+    this.result = result;
 }
 
 
-const calc1 = (createCalc("calc1", 2, 2, 3));
+const calc1 = new CreateCalc("calc1", 2, 2, 3);
 console.log(calc1);
