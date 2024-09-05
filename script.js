5a36f60581c8d0c7ce47cd3c494f50d851cc6a3c
Nathoggles <mawricus@gmail.com>
2024-09-05 04:58:21 +0200

    implement varying div populations depending on screen size

--- script.js
+++ script.js
@@ -25,53 +25,57 @@ function sum(calc) {
 sum(calc1);
 console.log(calc1);
 
-/*
-////div sketching
-function populateDivs(crossANumber) {
-    for (let i = 1; i <= crossANumber; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("class", "divs");
-        const width = allContainer.offsetWidth;
-        const height = allContainer.offsetHeight
-        console.log({width});
-        console.log({height});
-        div.style.flexBasis = `${100 / (crossANumber * 2)}%`;
-        //div.style.width = width / (6) + "px";
-        //div.style.height = height / (4) + "px";
-        //`${window.screen.width / (crossANumber / 4)}%`;
-        allContainer.appendChild(div);
-    }
- //   listener();
-}
-*/
-/*
-function populateDivs() {
-    for (let i = 1; i <= 66; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("class", "divs");
-        //div.setAttribute("id", "div" + i);
-        div.style.flexBasis = `${100 / 9}%`;
-        div.style.height = `${100 / 10}%`
-        allContainer.appendChild(div);     
-    }
-    const calculator = document.querySelector("#calculator");
-    const midDiv = document.querySelector("#div34");
-    console.log(midDiv);
-    allContainer.insertBefore(calculator, midDiv);
-}
-populateDivs();
 
-*/
+
+//make a layout depending on screen size
+
 let id = 0;
+
+const pageData = {};
+function getPageData() {
+    const width = window.innerWidth;
+    if (width < 600) {
+        pageData.rowCount = 5;
+        pageData.rowBasis = `${100 / 5}%`;
+        pageData.rowHeight = `${100 / 10}%`;
+        pageData.sideCount = 8;
+        pageData.sideBasis = `100%`;
+        pageData.sideHeight = `${100 / 8}%`;
+        console.table(pageData);
+    } 
+    else {
+    pageData.rowCount = 9;
+    pageData.rowBasis = `${100 / 9}%`;
+    pageData.rowHeight = `${100 / 10}%`;
+    pageData.sideCount = 24;
+    pageData.sideBasis = `${100 / 3}%`;
+    pageData.sideHeight = `${100 / 8}%`;
+
+    console.table(pageData);}
+};
+
+
+function makeDivs() {
+    const rC = pageData.rowCount;
+    const rB = pageData.rowBasis;
+    const rH = pageData.rowHeight;
+    const sC = pageData.sideCount;
+    const sB = pageData.sideBasis;
+    const sH = pageData.sideHeight;
+    populateRow(rC, rB, rH);
+    populateSideContainer(sC, sB, sH);
+    const calculator = document.querySelector("#calculator");
+    allContainer.appendChild(calculator);
+    populateSideContainer(sC, sB, sH);
+    populateRow(rC, rB, rH);
+}
 
 function populateRow(rowCount,rowBasis, rowHeight) {
     for (let i = 1; i <= rowCount; i++) {
         const div = document.createElement("div");
         id ++;
-        console.log(id);
         div.setAttribute("class", "divs");
         div.setAttribute("id", "div" + id);
-        console.log(div);
         div.style.flexBasis = rowBasis;
         div.style.height = rowHeight;
         allContainer.appendChild(div);  
@@ -94,44 +98,12 @@ function populateSideContainer(sideCount, sideBasis, sideHeight){
     } 
 }
 
-function populatePage() {
-    const width = window.innerWidth;
-    console.log({width});
-    if (width < 600) {
-        const rowCount = 5;
-        const rowBasis = `${100 / 5}%`;
-        const rowHeight = `${100 / 10}%`;
-        const sideCount = 8;
-        const sideBasis = `100%`;
-        const sideHeight = `${100 / 8}%`;
+getPageData();
+makeDivs();
 
-        makeDivs();
-    } 
-    const rowCount = 9;
-    const rowBasis = `${100 / 9}%`;
-    const rowHeight = `${100 / 10}%`;
-    const sideCount = 24;
-    const sideBasis = `${100 / 3}%`;
-    const sideHeight = `${100 / 8}%`;
 
-    makeDivs();
-}
 
-function makeDivs() {
-    populateRow();
-    populateSideContainer();
-    const calculator = document.querySelector("#calculator");
-    allContainer.appendChild(calculator);
-    populateSideContainer();
-    populateRow();
-}
 
-//populatePage();
 
-    const rowCount = 9;
-    const rowBasis = `${100 / 9}%`;
-    const rowHeight = `${100 / 10}%`;
-    const sideCount = 24;
-    const sideBasis = `${100 / 3}%`;
-    const sideHeight = `${100 / 8}%`;
+
     
