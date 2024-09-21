106a8e79b82da791cc3d9cc4d0ee5211ff9d8177
Nathoggles <mawricus@gmail.com>
2024-09-21 16:20:26 +0200

    replace element.style modifications in divs creation with adding CSS classes instead

--- script.js
+++ script.js
@@ -321,62 +321,52 @@ const pageData = {};
 function getPageData() {
     if (width < 1000) {
         pageData.rowCount = 5;
-        pageData.rowBasis = `${100 / 5}%`;
-        pageData.rowHeight = `${100 / 10}%`;
+        pageData.rowClass = "rowSmallscreen";
         pageData.sideCount = 8;
-        pageData.sideBasis = `100%`;
-        pageData.sideHeight = `${100 / 8}%`;
+        pageData.sideClass = "sideSmallscreen";
     } 
     else {
     pageData.rowCount = 9;
-    pageData.rowBasis = `${100 / 9}%`;
-    pageData.rowHeight = `${100 / 10}%`;
+    pageData.rowClass = "rowLargescreen";
+    pageData.sideClass = "sideLargescreen";
     pageData.sideCount = 24;
-    pageData.sideBasis = `${100 / 3}%`;
-    pageData.sideHeight = `${100 / 8}%`;
 }
 };
 
 function makeDivs() {
     const rC = pageData.rowCount;
-    const rB = pageData.rowBasis;
-    const rH = pageData.rowHeight;
     const sC = pageData.sideCount;
-    const sB = pageData.sideBasis;
-    const sH = pageData.sideHeight;
-    populateRow(rC, rB, rH);
-    populateSideContainer(sC, sB, sH);
+    populateRow(rC);
+    populateSideContainer(sC);
     const calculator = document.querySelector("#calculator");
     allContainer.appendChild(calculator);
-    populateSideContainer(sC, sB, sH);
-    populateRow(rC, rB, rH);
+    populateSideContainer(sC);
+    populateRow(rC);
 }
 
 
-function populateRow(rowCount,rowBasis, rowHeight) {
+function populateRow(rowCount) {
     for (let i = 1; i <= rowCount; i++) {
         const div = document.createElement("div");
         id ++;
-        div.setAttribute("class", "divs");
+        div.classList.add("divs");
         div.setAttribute("id", "div" + id);
-        div.style.flexBasis = rowBasis;
-        div.style.height = rowHeight;
+        div.classList.add(pageData.rowClass);
         allContainer.appendChild(div);  
         
     }
 }
 
-function populateSideContainer(sideCount, sideBasis, sideHeight){
+function populateSideContainer(sideCount){
     const sideContainer = document.createElement("div");
     sideContainer.setAttribute("class", "sideContainer");
     allContainer.appendChild(sideContainer);
     for (let i = 1; i <= sideCount; i++) {
         const div = document.createElement("div");
         id ++;
-        div.setAttribute("class", "divs");
+        div.classList.add("divs");
         div.setAttribute("id", "div" + id);
-        div.style.flexBasis = sideBasis;
-        div.style.height = sideHeight;
+        div.classList.add(pageData.sideClass);
         sideContainer.appendChild(div);    
     } 
 }
